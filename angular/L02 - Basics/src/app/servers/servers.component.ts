b9362ba17948cb3146df8abe0bf5644cdf1517a5
CZA - NOTE <cza@wmd.com.br>
2017-12-27 19:45:54 -0300

    Pasta renomeada.

--- angular/L02 - Basics/src/app/servers/servers.component.ts
+++ angular/L02 - Basics/src/app/servers/servers.component.ts
@@ -2,18 +2,31 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   //selector: '[app-servers]',
-  selector:'.app-servers',
-  template:`
-    <app-server></app-server>
-    <app-server></app-server>`,
+  selector: 'app-servers',
+  templateUrl: "./servers.component.html",
   styleUrls: ['./servers.component.css']
 
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'Nenhum servidor criado!';
+  serverName = 'Servidor Teste';
+
+  constructor() {
+    setTimeout(() => { this.allowNewServer = true; }, 2000)
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Servidor criado! O nome do servidor é '+this.serverName;
+  }
+
+  onUpdateServerName(event:any)
+  {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
